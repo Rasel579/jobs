9f9733f66128848947f7017041a27c224b909138
Ruslan <rasel579@gmail.com>
2019-03-27 01:01:04 +0500

    added a few dependecies for working with Indeed API

--- actions/jobActions.js
+++ actions/jobActions.js
@@ -1,7 +1,22 @@
 import axios from 'axios';
+import qs from 'qs';
 import { FETCH_JOBS } from './types';
 import reverseGeocode from 'latlng-to-zip';
 
+const JOB_QUERY_PARAMS = {
+    publisher: '4201738803816157',
+    format: 'json',
+    v: '2',
+    latlong: 1,
+    radius: 10,
+    q: 'javascript'
+};
+
 export const fetchJobs = (region) => async dispatch  => {
-          
+     try{
+        let zip = await  reverseGeocode(region);
+     }   catch(err){
+         console.error(err);
+     }
+    
    };
